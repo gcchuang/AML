96ca29b022ab9e43c76940edf3d768826e8bbb6c
weber <weber50432@gmail.com>
2023-06-26 13:03:43 +0800

    add TWCC version MIL training and test python script

---
+++ MIL_slide_level/MIL_test_TWCC.py
@@ -0,0 +1,134 @@
+import sys
+import os
+import numpy as np
+import argparse
+import random
+import PIL.Image as Image
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--lib', type=str, default='filelist', help='path to data file')
+parser.add_argument('--output', type=str, default='.', help='name of output directory')
+parser.add_argument('--model', type=str, default='', help='path to pretrained model')
+parser.add_argument('--batch_size', type=int, default=100, help='how many images to sample per slide (default: 100)')
+parser.add_argument('--workers', default=4, type=int, help='number of data loading workers (default: 4)')
+
+def main():
+    global args
+    args = parser.parse_args()
+
+    #load model
+    model = models.resnet34(True)
+    model.fc = nn.Linear(model.fc.in_features, 2)
+    ch = torch.load(args.model)
+    model.load_state_dict(ch['state_dict'])
+    model = model.cuda()
+    cudnn.benchmark = True
+
+    #normalization
+    normalize = transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.1,0.1,0.1])
+    trans = transforms.Compose([transforms.ToTensor(),normalize])
+
+    #load data
+    dset = MILdataset(args.lib, trans)
+    loader = torch.utils.data.DataLoader(
+        dset,
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=False)
+
+    dset.setmode(1)
+    probs = inference(loader, model)
+    maxs = group_max(np.array(dset.slideIDX), probs, len(dset.targets))
+
+    fp = open(os.path.join(args.output, 'predictions.csv'), 'w')
+    fp.write('file,target,prediction,probability\n')
+    for name, target, prob in zip(dset.slidenames, dset.targets, maxs):
+        fp.write('{},{},{},{}\n'.format(name, target, int(prob>=0.5), prob))
+    fp.close()
+
+def inference(loader, model):
+    model.eval()
+    probs = torch.FloatTensor(len(loader.dataset))
+    with torch.no_grad():
+        for i, input in enumerate(loader):
+            print('Batch: [{}/{}]'.format(i+1, len(loader)))
+            input = input.cuda()
+            output = F.softmax(model(input), dim=1)
+            probs[i*args.batch_size:i*args.batch_size+input.size(0)] = output.detach()[:,1].clone()
+    return probs.cpu().numpy()
+
+def group_max(groups, data, nmax):
+    out = np.empty(nmax)
+    out[:] = np.nan
+    order = np.lexsort((data, groups))
+    groups = groups[order]
+    data = data[order]
+    index = np.empty(len(groups), 'bool')
+    index[-1] = True
+    index[:-1] = groups[1:] != groups[:-1]
+    out[groups[index]] = data[index]
+    return list(out)
+
+class MILdataset(data.Dataset):
+    def __init__(self, libraryfile='', transform=None):
+        lib = torch.load(libraryfile)
+        print('')
+        #Flatten grid
+        grid = []
+        slideIDX = []
+        for i,g in enumerate(lib['grid']):
+            grid.extend(g)
+            slideIDX.extend([i]*len(g))
+
+        print('Number of tiles: {}'.format(len(grid)))
+        self.slidenames = lib['slides']
+        self.targets = lib['targets']
+        self.grid = grid
+        self.slideIDX = slideIDX
+        self.transform = transform
+        self.mode = None
+        self.mult = lib['mult']
+        self.size = int(np.round(224*lib['mult']))
+        self.level = lib['level']
+    def setmode(self,mode):
+        self.mode = mode
+    def maketraindata(self, idxs):
+        self.t_data = [(self.slideIDX[x],self.grid[x],self.targets[self.slideIDX[x]]) for x in idxs]
+    def shuffletraindata(self):
+        self.t_data = random.sample(self.t_data, len(self.t_data))
+    def __getitem__(self,index):
+        if self.mode == 1:
+            slide_num = self.slidenames[self.slideIDX[index]].split("_")[0]
+            slide_path = f"/staging/biology/b08611005/ROI_level0_pixel512/{slide_num}/"
+            patch_path = slide_path+ self.grid[index]
+            img =Image.open(patch_path).convert('RGB')
+            if self.mult != 1:
+                img = img.resize((224,224),Image.BILINEAR)
+            if self.transform is not None:
+                img = self.transform(img)
+            return img
+        elif self.mode == 2:
+            slideIDX, patch, target = self.t_data[index]
+            slide_num = self.slidenames[slideIDX].split("_")[0]
+            slide_path = f"/staging/biology/b08611005/ROI_level0_pixel512/{slide_num}/"
+            img = Image.open(slide_path+patch).convert('RGB')
+            if self.mult != 1:
+                img = img.resize((224,224),Image.BILINEAR)
+            if self.transform is not None:
+                img = self.transform(img)
+            return img, target
+    def __len__(self):
+        if self.mode == 1:
+            return len(self.grid)
+        elif self.mode == 2:
+            return len(self.t_data)
+
+if __name__ == '__main__':
+    main()
